8142ce8fd5bbb053b957d335a80b628551fdb0d5
Profª Andréa França <dejandreafranca@gmail.com>
2023-11-28 21:28:33 -0300

    Update createPoster.py

--- createPoster.py
+++ createPoster.py
@@ -7,4 +7,7 @@ img[0:240, 500:600] = rocket
 
 text = "Imagem1"
 
-cv2.putText(img,text,(20,220),fontFace = cv2.font,fontScale = 0.6,color = (130,110,45))
+cv2.putText(img,text,(20,220),fontFace = cv2.FONT_ITALIC,fontScale = 0.6,color = (130,110,45))
+
+cv2.imshow("Imagem",img)
+cv2.waitKey(0)
